42335d7035a5ce8c5cc2b0a2c4ae8c223b3c0092
Cyrex-TS <82237807+Cyrex-TS@users.noreply.github.com>
2021-04-09 16:43:43 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,65 @@
+const help = (prefix) => {
+	return `
+「 *Shan Bot* 」
+
+◪ *Información*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Creador: Shanduy
+  ❏ Como instalar el bot: https://youtu.be/rfxuYsWGmQo
+  ❏ Instagram: https://www.instagram.com/thepavos?r=nametag
+
+◪ *Para usar al bot*
+  ❏ Registrate con el comando ${prefix}daftar Nombre
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+
+◪ *CREAR*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  └─ ❏ ${prefix}tomp3 
+
+◪ *DESCARGAS*
+  │
+  └─ ❏ ${prefix}ytmp4
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ ❏ ${prefix}bahasa
+  └─ ❏ ${prefix}tts
+
+◪ *DUEÑO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex (Hernqn)
+  ├─ ❏ ${prefix}exe
+  └─ ❏ ${prefix}qrcode
+  
+◪ *NSWF*
+  │
+  └─ ❏ ${prefix}nsfwmenu
+
+     ❏ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw1 y para desactivar los NSFW coloque ${prefix}nsfw0
+
+No te olvides de seguirme en instagram flaco
+`
+}
+
+exports.help = help
